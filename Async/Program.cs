c8bef4de4ebce7e4deb30b0bf510981c0b2d1e1a
seifif <seifif@algonquincollege.com>
2022-06-18 21:12:20 -0400

    Initial version

---
+++ Async/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Async
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = await MainMenu();
+            }
+
+        }
+
+        private static async Task<bool> MainMenu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Sync");
+            Console.WriteLine("2) Non Blocking");
+            Console.WriteLine("3) Async");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    SyncBreakfast.Make();
+                    return true;
+                case "2":
+                    await NonBlockingBreakfast.MakeAsync();
+                    return true;
+                case "3":
+                    await AsyncBreakfast.MakeAsync();
+                    return true;
+                default:
+                    return true;
+            }
+        }
+    }
+}
